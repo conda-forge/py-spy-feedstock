d7a7b8cfc5b7538e4663e646b6b1b947a241a17e
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-27 15:53:02 +0200

    Add PREFIX to link path

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,9 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 if [ $(uname) = Darwin ] ; then
-  export RUSTFLAGS="-C link-args=-Wl,-rpath,$PREFIX/lib"
+  export RUSTFLAGS="-C link-args=-Wl,-rpath,${PREFIX}/lib"
 else
-  export RUSTFLAGS="-C link-args=-Wl,-rpath-link,$PREFIX/lib"
+  export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,${PREFIX}/lib -C link-arg=-L${PREFIX}"
 fi
 
 # build statically linked binary with Rust
